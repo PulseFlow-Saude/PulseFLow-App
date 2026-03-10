41741ca765551730998fbbb99e2192fbc769856b
Henrique Ribeiro <henrique@proansi.com.br>
2026-03-10 17:54:00 -0300

    Ajuste

--- lib/config/database_config_io.dart
+++
@@ -1,4 +0,0 @@
-/// MongoDB Atlas - conexão direta na nuvem (funciona em qualquer dispositivo).
-String getConnectionString() {
-  return 'mongodb+srv://pulseflow:projetointegrador@pulseflow.uesi5bb.mongodb.net/pulseflow?retryWrites=true&w=majority';
-}
--- lib/config/database_config_stub.dart
+++
@@ -1,4 +0,0 @@
-/// Stub para web (dart:io não disponível).
-String getConnectionString() {
-  return 'mongodb+srv://pulseflow:projetointegrador@pulseflow.uesi5bb.mongodb.net/pulseflow?retryWrites=true&w=majority';
-}
